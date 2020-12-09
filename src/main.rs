783e6e52b11e79b151e619c3367e602674dcb843
Marko Mikulicic <mmikulicic@gmail.com>
2020-12-09 11:52:49 +0100

    Include hash and size

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,39 @@
 extern crate rocket;
 
 use rocket::response::Redirect;
+use text_io::try_scan;
+use anyhow::Result;
 
 const IPFS_GATEWAY: &str = "ipfs.io";
-const EMPTY_OBJECT_DIGEST: &str = "sha256:44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a";
+const EMPTY_OBJECT_DIGEST: &str =
+    "sha256:44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a";
 const EMPTY_OBJECT_CID: &str = "QmbJWAESqCsf4RFCqEY7jecCashj8usXiyDNfKtZCwwzGb";
 
 #[get("/")]
 fn root() {}
 
 #[get("/layer/<repo>/manifests/<tag>")]
-fn manifests(repo: String, tag: String) -> String {
-    println!("manifest repo={}, digest={}!", repo, tag);
-    r#"{
+fn manifests(repo: String, tag: String) -> Result<String> {
+	let size: i64;
+	let sha: String;
+	try_scan!(tag.bytes() => "{}-{}", size, sha);
+
+    println!("manifest repo={}, tag={}, size={}, sha={}!", repo, tag, size, sha);
+    Ok(format!(r#"{{
   "schemaVersion": 2,
-  "config": {
+  "config": {{
     "mediaType": "application/vnd.dummy.config.v1+json",
     "digest": "sha256:44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a",
-    "size": 622
-  },
+    "size": 2
+  }},
   "layers": [
-    {
+    {{
       "mediaType": "application/vnd.oci.image.layer.v1.tar+gzip",
-      "digest": "sha256:0000000000000000000000000000000000000000000000000000000000000000",
-      "size": 168
-    }
+      "digest": "sha256:{}",
+      "size": {}
+    }}
   ]
-}"#.to_string()
+}}"#, sha, size))
 }
 
 #[get("/layer/<repo>/blobs/<digest>")]
