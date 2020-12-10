cbc77e2f4c72ab41334c1e7625fe012321c3c86a
Marko Mikulicic <mmikulicic@gmail.com>
2020-12-10 17:29:02 +0100

    Cleanup some rust

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,17 @@
 extern crate rocket;
 
 use anyhow::Result;
+use count_write::CountWrite;
 use err_derive::Error;
 use reqwest::blocking;
 use rocket::response::Redirect;
 use rocket_contrib::json::Json;
 use serde::{Deserialize, Serialize};
-use sha2::{Digest, Sha256, Sha512};
+use sha2::{Digest as Sha2Digest, Sha256};
 use std::collections::HashMap;
 use std::convert::TryFrom;
 use std::ffi::{OsStr, OsString};
+use std::fmt;
 use std::path::PathBuf;
 use text_io::try_scan;
 
@@ -110,7 +112,7 @@ struct RootFS {
 struct Descriptor {
     media_type: String,
     digest: String,
-    size: usize,
+    size: u64,
     annotations: HashMap<String, String>,
 }
 
@@ -130,9 +132,11 @@ struct ImageManifest {
     layers: Vec<Descriptor>,
 }
 
-impl LayerManifest {
-    fn config(&self) -> Result<ImageConfig> {
-        let tsha = self
+impl TryFrom<&LayerManifest> for ImageConfig {
+    type Error = anyhow::Error;
+
+    fn try_from(lm: &LayerManifest) -> Result<Self, Self::Error> {
+        let tsha = lm
             .layer
             .annotations
             .get("io.ocipfs.layer.fs.digest")
@@ -161,14 +165,14 @@ fn manifests<'a>(cid: CID, _tag: &'a str) -> Result<Json<ImageManifest>> {
     let lman = LayerManifest::try_from(&cid)?;
     println!("Manifest = {:?}", &lman);
 
-    let cfg = lman.config()?;
+    let digest = Digest::new(&ImageConfig::try_from(&lman)?)?;
     let iman = ImageManifest {
         schema_version: 2,
         media_type: "application/vnd.oci.image.manifest.v1+json".to_string(),
         config: Descriptor {
             media_type: "application/vnd.oci.image.config.v1+json".to_string(),
-            size: serde_json::to_vec(&cfg)?.len(),
-            digest: mkdigest(&cfg)?,
+            size: digest.size,
+            digest: digest.to_string(),
             annotations: HashMap::new(),
         },
         layers: vec![lman.layer],
@@ -205,8 +209,9 @@ fn blobs<'a>(cid: CID, digest: &'a str) -> Result<BlobsResult> {
         let u = format!("https://{}/ipfs/{}", IPFS_GATEWAY, lcid);
         Ok(Tarball(Redirect::found(u)))
     } else {
-        let cfg = lman.config()?;
-        if digest == mkdigest(&cfg)? {
+        let cfg = ImageConfig::try_from(&lman)?;
+        let d = Digest::new(&cfg)?;
+        if digest == d.to_string() {
             Ok(Config(Json(cfg)))
         } else {
             Err(BlobsError::UnknownManifest)?
@@ -214,11 +219,29 @@ fn blobs<'a>(cid: CID, digest: &'a str) -> Result<BlobsResult> {
     }
 }
 
-fn mkdigest<T: Serialize>(v: T) -> Result<String> {
-    let mut hasher = Sha256::new();
-    serde_json::to_writer(&mut hasher, &v)?;
-    let result = hasher.finalize();
-    Ok(format!("sha256:{:x}", result))
+struct Digest {
+    hash: generic_array::GenericArray<u8, <Sha256 as Sha2Digest>::OutputSize>,
+    size: u64,
+}
+
+impl Digest {
+    #[allow(dead_code)]
+    fn new<T: Serialize>(v: T) -> Result<Self> {
+        let mut hasher = Sha256::new();
+        let size = {
+            let mut cw = CountWrite::from(&mut hasher);
+            serde_json::to_writer(&mut cw, &v)?;
+            cw.count()
+        };
+        let hash = hasher.finalize();
+        Ok(Digest { hash, size })
+    }
+}
+
+impl fmt::Display for Digest {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "sha256:{:x}", self.hash)
+    }
 }
 
 #[get("/")]
